65272f671864b6c988047ec22fa62d5d2b8e0f94
kc-hsh-value <mapedeveloper@protonmail.com>
2025-11-08 01:48:33 -0800

    first commit

--- src/providers/providers.tsx
+++ src/providers/providers.tsx
@@ -2,26 +2,42 @@
 
 import { PrivyProvider } from "@privy-io/react-auth";
 import { toSolanaWalletConnectors } from "@privy-io/react-auth/solana";
+import { arbitrum, base, polygon } from "viem/chains";
+// import { ThemeProvider } from "./theme-provider";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <PrivyProvider
       appId={process.env.NEXT_PUBLIC_PRIVY_APP_ID!}
-      clientId={process.env.NEXT_PUBLIC_PRIVY_CLIENT_ID!}
+      // clientId={process.env.NEXT_PUBLIC_PRIVY_CLIENT_ID!}
       config={{
+        // Create embedded wallets for users who don't have a wallet
         embeddedWallets: {
           ethereum: {
-            createOnLogin: "users-without-wallets",
+            createOnLogin: 'all-users'
           },
           solana: {
-            createOnLogin: "users-without-wallets",
-          },
+            createOnLogin: 'all-users'
+          }
         },
-        appearance: { walletChainType: "ethereum-and-solana" },
-        externalWallets: { solana: { connectors: toSolanaWalletConnectors() } },
+        appearance:{
+            // logo:"",
+            
+        },
+        defaultChain: polygon,
+        supportedChains:[polygon, base, arbitrum],
+        solanaClusters: [{name: 'mainnet-beta', rpcUrl: 'https://api.mainnet-beta.solana.com'}],
       }}
     >
-      {children}
+      {/* <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          > */}
+
+        {children}
+      {/* </ThemeProvider> */}
     </PrivyProvider>
   );
 }
